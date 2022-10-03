bae5f34597fcfb849bca450321b2647343461bbf
simopek <>
2022-10-03 23:36:55 +0200

    Add creation and save into memory of notes

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 use std::{collections::HashMap, io};
 
 fn main() {
-    let mut notes: HashMap<&str, &str> = HashMap::new();
+    let mut notes: HashMap<String, String> = HashMap::new();
 
     loop {
         println!("next command?");
@@ -18,7 +18,7 @@ fn main() {
     }
 }
 
-fn handle_command(command: &str, notes: &mut HashMap<&str, &str>) {
+fn handle_command(command: &str, notes: &mut HashMap<String, String>) {
     match command {
         "add" => handle_add_note_command(notes),
         "ls" => handle_list_notes_command(notes),
@@ -28,10 +28,32 @@ fn handle_command(command: &str, notes: &mut HashMap<&str, &str>) {
     }
 }
 
-fn handle_add_note_command(notes: &mut HashMap<&str, &str>) {}
+fn handle_add_note_command(notes: &mut HashMap<String, String>) {
+    println!("write the note");
 
-fn handle_list_notes_command(notes: &HashMap<&str, &str>) {}
+    let mut new_note_content = String::new();
+    let mut content_ok = false;
+
+    while !content_ok {
+        match io::stdin().read_line(&mut new_note_content) {
+            Ok(0) => {}
+            Ok(_) => {
+                content_ok = true;
+            }
+            Err(_) => {}
+        }
+    }
+
+    new_note_content.pop();
+    notes.insert(random_string::generate(6, "123456789"), new_note_content);
+}
+
+fn handle_list_notes_command(notes: &HashMap<String, String>) {
+    for (id, content) in notes.iter() {
+        println!("id -> {id}, content -> {content}");
+    }
+}
 
 fn handle_help_command() {}
 
-fn handle_rm_note_command(notes: &mut HashMap<&str, &str>) {}
+fn handle_rm_note_command(notes: &mut HashMap<String, String>) {}
